75539657f34b69cf2368407205a3643fe18ab97a
brownec <cbrowne77@gmail.com>
2015-04-21 01:36:40 +0100

    final commit of the night

--- MonthlyBudgetCalculator/MonthlyBudgetCalculator/Controllers/BudgetsController.cs
+++ MonthlyBudgetCalculator/MonthlyBudgetCalculator/Controllers/BudgetsController.cs
@@ -8,8 +8,11 @@ using System.Web;
 using System.Web.Mvc;
 using MonthlyBudgetCalculator.Models;
 using DotNet.Highcharts;
-using DotNet.Highcharts.Options;
+using DotNet.Highcharts.Enums;
 using DotNet.Highcharts.Helpers;
+using DotNet.Highcharts.Options;
+using Point = DotNet.Highcharts.Options.Point;
+using System.Drawing;
 
 namespace MonthlyBudgetCalculator.Controllers
 {
@@ -137,7 +140,7 @@ namespace MonthlyBudgetCalculator.Controllers
             return RedirectToAction("Index");
         }
 
-         // Budget Summary View
+        // ******************** BUDGET ANALYSIS SUMMARY ********************
         public ActionResult Summary(int? id)
         {
             Budget b = new Budget();
@@ -275,6 +278,7 @@ namespace MonthlyBudgetCalculator.Controllers
             return View(b);
         }
 
+        // ******************** BUDGET ANALYSIS CHARTS ********************
         public ActionResult Charts(int? id)
         {
             Budget b = new Budget();
@@ -283,20 +287,138 @@ namespace MonthlyBudgetCalculator.Controllers
 
             // DotNet.Highcharts.Highcharts chart = new DotNet.Highcharts.Highcharts("chart")
             Highcharts chart = new Highcharts("chart")
-            .SetCredits(new Credits { Enabled = false}) // remove hyperlink for highchart
-            .SetTooltip(new Tooltip { Crosshairs = new Crosshairs(true, true)})
-            .SetTitle(new Title { Text = "Monthly Budget: " + b.BudgetName})
-            .SetSubtitle(new Subtitle { Text = " Budget Analysis Chart "})
-                .SetXAxis(new XAxis
+
+            .InitChart(new Chart
+            {
+                DefaultSeriesType = ChartTypes.Line,
+                MarginRight = 130,
+                MarginBottom = 25,
+                ClassName = "chart"
+            })
+
+
+
+            .SetTitle(new Title
+            {
+                Text = "Monthly Budget: " + b.BudgetName
+            })
+
+            .SetSubtitle(new Subtitle
+            {
+                Text = " Budget Analysis Chart "
+            })
+
+            .SetXAxis(new XAxis
+            {
+                Categories = new[] { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" }
+            })
+
+            .SetYAxis(new YAxis
+            {
+                Title = new YAxisTitle
                 {
-                    Categories = new[] { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" }
-                })
-                .SetSeries(new Series
-                {
-                    Data = new Data(new object[] { 29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4 })
-                });
+                    Text = "TEXT HERE"
+                },
+                PlotLines = new[]
+                { 
+                    new YAxisPlotLines
+                    {
+                        Value = 0,
+                        Width = 1,
+                        Color = ColorTranslator.FromHtml("#808080")
+                    }
+                }
+            })
+
+            .SetTooltip(new Tooltip
+            {
+                Crosshairs = new Crosshairs(true, true)
+            })
+
+            .SetLegend(new Legend
+            {
+                Layout = Layouts.Vertical,
+                Align = HorizontalAligns.Right,
+                VerticalAlign = VerticalAligns.Top,
+                X = -10,
+                Y = 100,
+                BorderWidth = 0
+            })
+
+            .SetSeries(new Series
+            {
+                Data = new Data(new object[] { 29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4 })
+            })
+
+            .SetCredits(new Credits
+            {
+                Enabled = false
+            }); // remove hyperlink for highchart
 
             return View(chart);
+        }
+
+        // ******************** BUDGET ANALYSIS FORECAST ********************
+        public ActionResult Forecast(int? id)
+        {
+            Budget b = new Budget();
+            // return list of budgets specific to one user
+            b = db.Budgets.Where(user => user.BudgetUserId == id).SingleOrDefault();
+
+            // -------------------- SELECTION SORT --------------------
+            // -------------------- BUDGET BALANCES --------------------
+            /* idea to sort the list of budget balances and display top 5
+             * based on whether they highest balances or lowest balances
+            */
+            
+            //int upper, inner;
+            //int minimumBudgetBalance, tempBudgetBalance;
+            //for(int outer = 0; outer <= upper; outer ++)
+            //{
+            //    minimumBudgetBalance = outer;
+            //    for(int inner = outer + 1; inner <= upper; inner ++ )
+            //    if(b[inner] < b[minimumBudgetBalance])
+            //        minimumBudgetBalance = inner;
+            //    tempBudgetBalance = b[outer];
+            //    b[outer] = b[minimumBudgetBalance];
+            //    b[minimumBudgetBalance] = tempBudgetBalance;
+            //    return minimumBudgetBalance;
+            //}
+
+            // -------------------- SEARCHING ALGORITHM --------------------
+            // -------------------- MINIMUM BUDGET BALANCE --------------------
+            /* idea to return budget balances with lowest balance
+               and display
+             * i)assign 1st element in array as minimum value
+             * ii)loop through array and compare each element in the array
+             * iii)if accessed variable value less than current minimum value, replace
+            */
+            //static int FindMinimumBudgetBalance(int[] b) // b is array(maybe change)
+            //{
+            //    int minimumBudgetBalance = b[0];
+            //    for(int i = 0; i < b.Length - 1; i++)
+            //        if(b[index] < minimumBudgetBalance)
+            //            minimumBudgetBalance = b[index];
+            //    return minimumBudgetBalance;
+            //}
+
+            // -------------------- MAXIMUM BUDGET BALANCE --------------------
+            /* idea to return budget balances with highest balance
+               and display
+             * i)assign 1st array element to variable that stores maximum value
+             * ii)loop through array comparing each element with value stored in variable
+             * iii)replace current value if accessed value is greater
+            */
+            //static int FindMaximumBudgetBalance(int[] b)
+            //{
+            //    int maximumBudgetBalance = b[0];
+            //    for(int i = 0; i < b.Length - 1; i++)
+            //        if(b[index] > maximumBudgetBalance)
+            //            maximumBudgetBalance = b[index];
+            //    return maximumBudgetBalance;
+            //}
+            
+            return View(b);
         }
 
         protected override void Dispose(bool disposing)
